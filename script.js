522bc89df89701375a0be03c831a29ba18093a81
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-12 14:52:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let s="#abc"
+	if (s.length===4) {
+		let ans=`# ${s[i]}${s[i]} ${s[2]}${s[2]} ${s[3]}${s[3]}`
+		console.log(ans)
+	}
+	
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
