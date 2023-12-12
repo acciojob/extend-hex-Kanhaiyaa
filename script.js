f8c9ede310d9697b8ce69a6c54b3f4ec2fe18d5d
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-12 14:52:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const extendHex = (shortHex) => {
   // write your code here
 	let s="#abc"
 	if (s.length===4) {
-		let ans=`# ${s[i]}${s[i]} ${s[2]}${s[2]} ${s[3]}${s[3]}`
+		let ans=`# ${s[1]}${s[1]} ${s[2]}${s[2]} ${s[3]}${s[3]}`
 		console.log(ans)
 	}
 	
